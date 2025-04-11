af24ae80b1f94ea2571605e1297666e593772a08
ayaanqui <ayaanqui.com@gmail.com>
2025-04-11 14:43:38 -0500

    init spring app

---
+++ src/main/java/com/pricetra/email_server/EmailServerApplication.java
@@ -0,0 +1,18 @@
+package com.pricetra.email_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailServerApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello"
+	}
+
+}
---
+++ src/test/java/com/pricetra/email_server/EmailServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pricetra.email_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
