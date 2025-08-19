d656b44be7d77f77bb1ec18179548feaa4cf3ba6
ayaanqui <ayaanqui.com@gmail.com>
2025-08-19 00:12:47 -0500

    feat: init phoenix project

--- src/main/java/com/pricetra/email_server/dto/PasswordResetRequest.java
+++
@@ -1,7 +0,0 @@
-package com.pricetra.email_server.dto;
-
-public class PasswordResetRequest extends EmailRequest {
-    public String code;
-    public String fullName;
-    public String avatarUrl;
-}
