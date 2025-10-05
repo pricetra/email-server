a0ae648c78ea1f553494797fd13f4b63921cacf3
ayaanqui <ayaanqui.com@gmail.com>
2025-10-05 14:35:59 -0500

    feat: skip auth on api-docs route

--- src/middleware/jwt-auth.ts
+++ src/middleware/jwt-auth.ts
@@ -2,6 +2,8 @@ import type { Request, Response, NextFunction } from "express";
 import jsonwebtoken from 'jsonwebtoken';
 
 export default function jwtAuth(req: Request, res: Response, next: NextFunction) {
+  if (process.env.ENV !== 'production' && req.path === '/api-docs') return next();
+
   const authValue = req.headers.authorization
   if (!authValue) {
     res.status(401).json({ message: 'unauthorized' });
