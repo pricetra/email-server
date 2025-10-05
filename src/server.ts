828841f6b84494d1c840883f882f793d34c7cd0a
ayaanqui <ayaanqui.com@gmail.com>
2025-10-04 23:37:52 -0500

    feat: setup auth middleware

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,26 @@
 import express from 'express';
+import jsonwebtoken from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: '.env' })
+
+const JWT_KEY = process.env.JWT_KEY;
+if (!JWT_KEY) throw new Error('JWT_KEY required')
 
 const app = express()
 const port = 3001
+
+app.use((req, res, next) => {
+  const authValue = req.headers.authorization
+  if (!authValue) throw new Error('unauthorized');
+
+  const parsedAuthValue = authValue.split(' ');
+  if (parsedAuthValue.length !== 2 || parsedAuthValue.at(0) !== 'Bearer') throw new Error('unauthorized');
+
+  const jwt = parsedAuthValue.at(1)!;
+  if (jsonwebtoken.verify(jwt, JWT_KEY)) { }
+  return next();
+})
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
